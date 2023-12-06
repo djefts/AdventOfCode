35ad88c9683e946a51f527044adcb15406fd11b8
David Jefts <30559436+djefts@users.noreply.github.com>
2023-12-06 00:20:21 -0600

    Initial Day 6 Solutions

---
+++ 2023/day06/task.py
@@ -0,0 +1,49 @@
+"""
+
+"""
+
+
+def task1(input_lines: list[str]):
+    """
+    
+    """
+    times = input_lines[0].split(":")[1].split()
+    records = input_lines[1].split(":")[1].split()
+    assert len(times) == len(records)
+    races = len(times)
+    
+    prod = 1
+    for race in range(races):
+        time = int(times[race])
+        record = int(records[race])
+        ways_to_win = 0
+        for i in range(1, time - 1):
+            held = i
+            speed = held
+            distance = speed * (time - held)
+            if distance > record:
+                ways_to_win += 1
+        prod *= ways_to_win
+    print(prod)
+
+
+"""
+
+"""
+
+
+def task2(input_lines: list[str]):
+    """
+    
+    """
+    time = int(''.join(input_lines[0].split(":")[1].split()))
+    record = int(''.join(input_lines[1].split(":")[1].split()))
+    
+    ways_to_win = 0
+    for i in range(1, time - 1):
+        held = i
+        speed = held
+        distance = speed * (time - held)
+        if distance > record:
+            ways_to_win += 1
+    print(ways_to_win)
